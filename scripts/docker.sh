91c29d1b9635960153c17f91ec5519c8be38270c
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-09-06 17:08:03 +0530

    Docker: Updates image to run Python and R jobs

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 # Run scheduler server inside docker container
-## TODO: Move environment variables to docker compose file
 source /venv/bin/activate && \
-	NDSCHEDULER_SETTINGS_MODULE=scheduler.settings.production \
-	python /root/scheduler/scheduler.py
+	python /root/ndscheduler/scheduler/scheduler.py

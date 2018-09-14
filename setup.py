157dd0749a4dca363bbd05f55a1b4dd595b29e2d
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2018-09-14 13:30:07 +0530

    Use latest version of apscheduler

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     ],
     test_suite='nose.collector',
     install_requires=[
-        'APScheduler == 3.0.0',
+        'APScheduler == 3.5.3',
         'SQLAlchemy == 1.2.11',
         'future == 0.15.2',
         'tornado == 4.3.0',
